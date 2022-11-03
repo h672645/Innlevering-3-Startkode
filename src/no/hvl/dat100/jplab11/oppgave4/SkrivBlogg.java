68bfa4d0dda1d2bd2e3d3de11531a50dde8df990
h672645 <672645@stud.hvl.no>
2022-11-03 17:48:36 +0100

    Update SkrivBlogg.java

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -2,14 +2,28 @@ package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.File;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+	public static boolean skriv(Blogg samling, String mappe, String filnavn) throws Exception {
 
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter skriver = new PrintWriter(filnavn, mappe);
+		
+		try{
+			skriver.print(samling.toString());
+			
+			skriver.close();
+		} catch(Exception e) {
+			e.getStackTrace();
+		}
+		
+		
+
+		return true;
+
 	}
 }
